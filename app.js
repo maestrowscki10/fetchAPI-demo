f1d929a6c9ed92b27fe0d9a74473fcb41d5b3c68
maestrowscki10 <abdulhansen.ah@gmail.com>
2022-04-03 16:06:02 +0300

    some changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('button1').addEventListener('click',messi)
-
+document.getElementById('button2').addEventListener('click',appJson)
 function messi(){
     fetch('app.text')
     .then(function (res) {
@@ -7,6 +7,31 @@ function messi(){
     })
         .then(function (data) {
             console.log(data);
+            document.getElementById('output').innerHTML=data
         })
-      
+      .catch(function(err) {
+          console.log(err);
+      })
 }
+
+
+function appJson(){
+    fetch('app.json')
+    .then(function (res) {
+        return res.json();
+    })
+        .then(function (data) {
+            console.log(data);
+            let output =''
+        data.forEach(post => {
+                output+=`<li>${post.title}</li>`
+                output+=`<li>${post.body}</li>`
+            });
+            document.getElementById('output').innerHTML=output
+        })
+      .catch(function(err) {
+          console.log(err);
+      })
+}
+
+
